482571db76e7f887de801c15e3c3990335bbb39a
Terrell Murrell <littlerell12@gmail.com>
2020-07-27 23:52:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {Ariana: 'God is a Woman'};
 
 function updatePlaylist() {
-  playlist['Lady Gaga'] = 'Applause';
+  playlist['Slowdive'] = 'Alison'
+  playlist['My Bloody Valentine'] = 'Sometimes'
   return playlist;
 }
